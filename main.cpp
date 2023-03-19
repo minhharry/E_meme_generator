bfad7e4bcfb00885f5b1a73eca8c6702f3bc0723
Minh <tbminh@gmail.com>
2023-03-19 19:57:37 +0700

    update and fix some bug

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 map<string, int> mp;
 vector<string> a;
-const string E_MEME = "UwU";
+const string E_MEME = "e";
 const string randstr = "oqn4234c2a37289";
+const bool LongE = true;
+bool cmp(pair<string, int> a, pair<string, int> b)
+{
+    if (a.second==b.second)
+    {
+        return (a.first < b.first);
+    }
+    return (a.second > b.second);
+}
 int main ()
 {
     freopen("debug.inp","r",stdin);
@@ -22,8 +31,6 @@ int main ()
             s = regex_replace(s,regex(";"), " ; ");
             s = regex_replace(s,regex(","), " , ");
             s = regex_replace(s,regex("\\."), " . ");
-            s = regex_replace(s,regex("\\*"), " * ");
-            s = regex_replace(s,regex("\\/"), " / ");
             s = regex_replace(s,regex("\\("), " ( ");
             s = regex_replace(s,regex("\\)"), " ) ");
             s = regex_replace(s,regex("\\["), " [ ");
@@ -31,77 +38,90 @@ int main ()
             s = regex_replace(s,regex("\\{"), " { ");
             s = regex_replace(s,regex("\\}"), " } ");
 
-            s = regex_replace(s,regex("\\+\\+"), randstr);
-            s = regex_replace(s,regex("\\+"), " + ");
-            s = regex_replace(s,regex(randstr), " ++ ");
+            s = regex_replace(s,regex("\\+\\+"), " ++ ");
 
-            s = regex_replace(s,regex("\\-\\-"), randstr);
-            s = regex_replace(s,regex("\\-"), " - ");
-            s = regex_replace(s,regex(randstr), " -- ");
+            s = regex_replace(s,regex("\\-\\-"), " -- ");
+
 
             s = regex_replace(s,regex("::"), randstr);
             s = regex_replace(s,regex(":"), " : ");
             s = regex_replace(s,regex(randstr), " :: ");
 
-            s = regex_replace(s,regex("<<"), randstr);
-            s = regex_replace(s,regex("<"), " < ");
-            s = regex_replace(s,regex(randstr), " << ");
 
-            s = regex_replace(s,regex(">>"), randstr);
-            s = regex_replace(s,regex(">"), " > ");
-            s = regex_replace(s,regex(randstr), " >> ");
+            s = regex_replace(s,regex("=="), " == ");
+            s = regex_replace(s,regex("!="), " != ");
+            s = regex_replace(s,regex("<<"), " << ");
+            s = regex_replace(s,regex(">>"), " >> ");
+            s = regex_replace(s,regex("//"), " // ");
+            s = regex_replace(s,regex("&&"), " && ");
+            s = regex_replace(s,regex("\\|\\|"), " || ");
+            s = regex_replace(s, regex("\"([^\"]*)\""), " \"$1\" ");
+            s = regex_replace(s, regex("'([^']*)'"), " '$1' ");
+
             stringstream ss(s);
             string x = "";
+            string xx = "";
             while (ss >> x)
             {
-                if (x[0]=='"')
+                xx = x;
+                char quote = 0;
+                if (x[0]=='\"') quote = '\"';
+                if (x[0]=='\'') quote = '\'';
+                if (x[0]==quote)
                 {
-                    size_t pos = s2.find("\""); // find first quotation mark
-                    s2[pos] = '_';
+                    string temp = x;
+                    size_t pos = s2.find(quote); // find first quotation mark
+                    if (pos != std::string::npos)
+                        s2[pos] = '_';
                     if (pos != std::string::npos)
                     {
-                        size_t end_pos = s2.find("\"", pos + 1); // find second quotation mark
+                        size_t end_pos = s2.find(quote, pos + 1); // find second quotation mark
                         if (end_pos != std::string::npos)
                         {
                             x = s2.substr(pos + 1, end_pos - pos - 1); // extract the substring
+                            s2[end_pos] = '_';
                         }
-                        s2[end_pos] = '_';
                     }
-                    x = '"' + x + '"';
-                    string temp;
-                    ss >> temp;
-                    while (temp[temp.size()-1]!='"' && (ss >> temp));
+                    xx = quote + x + quote;
+                    if (temp=="") continue;
+                    pos = temp.find(quote); // find first quotation mark
+                    if (pos != std::string::npos)
+                        temp[pos] = '_';
+                    while (temp.find(quote)==string::npos && (ss >> temp));
                 }
-                if (x=="") continue;
-                ++mp[x];
-                a.push_back(x);
+                if (xx=="") continue;
+                ++mp[xx];
+                a.push_back(xx);
             }
             a.push_back("\n");
         }
     }
-    vector<pair<int, string>> b;
+    vector<pair<string, int>> b;
     for (map<string, int>::iterator it = mp.begin(); it != mp.end(); ++it)
     {
-        b.push_back(make_pair(it->second,it->first));
+        b.push_back(make_pair(it->first,it->second));
     }
 
 
 
 
-    sort(b.begin(), b.end());
-    reverse(b.begin(), b.end());
+    sort(b.begin(), b.end(), cmp);
     //srand(time(NULL));
     //random_shuffle(b.begin(), b.end());
 
 
     for (int i = 0; i<b.size(); ++i)
     {
-        mp[b[i].second] = i+1;
-        cerr << b[i].second << ' ' << i+1 << endl;
+        mp[b[i].first] = i+1;
+        cerr << b[i].first << ' ' << i+1 << endl;
         cout << "#define ";
-        //////////cout << 'e' << i+1 << ' ';
-        for (int j = 0; j<i+1; ++j) cout << E_MEME;
-        cout << ' ' << b[i].second << endl;
+        if (!LongE)
+            cout << 'e' << i+1 << ' ';
+        else
+        {
+            for (int j = 0; j<i+1; ++j) cout << E_MEME;
+        }
+        cout << ' ' << b[i].first << endl;
     }
 
     for (int i = 0; i<a.size(); ++i)
@@ -111,14 +131,21 @@ int main ()
             cout << endl;
             continue;
         }
-        for (int j = 0; j<mp[a[i]]; ++j) cout << E_MEME;
-        ////////////cout << 'e' << mp[a[i]];
+        if (a[i]=="//")
+        {
+            cout << "// ";
+            continue;
+        }
+        if (!LongE)
+            cout << 'e' << mp[a[i]];
+        else
+            for (int j = 0; j<mp[a[i]]; ++j) cout << E_MEME;
         cout << ' ';// cout << a[i]; cout << ' ';
     }
-   /* for (int i = 0; i<a.size(); ++i)
-    {
-        cout << a[i] <<  ' ';
-    }*/
+    /* for (int i = 0; i<a.size(); ++i)
+     {
+         cout << a[i] <<  ' ';
+     }*/
     return 0;
 }
 
